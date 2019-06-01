e2f5b23531ac3559fa749f9529703642640ccdb5
Pedro Henrique Silva Mietlicki <pedromietlicke@gmail.com>
2019-06-01 16:19:54 -0300

    Colocando Menu inicial

--- index.php
+++ index.php
@@ -1,13 +1,30 @@
 <?php?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>AmostraIF</title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
+
 <body>
-    
+    <header >
+       <ul class="menu">
+           <li class="textVerde"><h1>Amostra<span class="textoVermelho">IF</span></h1></li>
+           <li><a href="#">Home</a></li>
+           <li><a href="#">Sobre</a></li>
+           <li><a href="#">login</a>
+            <ul>
+                <li><a href="#">Professor</a></li>
+                <li><a href="#">Aluno</a></li>
+            </ul>
+            </li>
+       </ul>
+    </header>
+
 </body>
+
 </html>
